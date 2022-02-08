078c7bb03aff5c442e1a42eb61d82fc69663eae9
mpoulhazan <mp041@hotmail.fr>
2022-02-07 23:21:34 -0500

    Add layers

--- src/config.js
+++ src/config.js
@@ -5,28 +5,27 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "CryptoEgg";
-const description = "What's inside ? Nobody knows — 10.000 unique hand-drawn and programmatically generated pixel art Eggs. Be ready for the multiverse ! These eggs may hold some surprises for you....";
+const namePrefix = 'QNFTip';
+const description = 'Q-NFTip collecion';
 
 //IMAGE URL
-const baseUri = "ipfs://QmXGtc5CrjweLMXtKxBiX1iit48qtpBrWgeHKYcdDFXad2";
-
+const baseUri = 'ipfs://QmXGtc5CrjweLMXtKxBiX1iit48qtpBrWgeHKYcdDFXad2';
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
-  {
-    growEditionSizeTo: 10000,
-    layersOrder: [
-      { name: "Background" },
-      { name: "Neck" },
-      { name: "Egg" },
-       { name: "Body" },
-       { name: "Eyes" },
-       { name: "Glasses" },
-      { name: "Hair" },
-       { name: "Mouth" },
-    ],
-  },
+    {
+        growEditionSizeTo: 10000,
+        layersOrder: [
+            { name: 'Background' },
+            { name: 'Neck' },
+            { name: 'Qtip' },
+            { name: 'Body' },
+            { name: 'Eyes' },
+            { name: 'Glasses' },
+            { name: 'Hair' },
+            { name: 'Mouth' },
+        ],
+    },
 ];
 
 const shuffleLayerConfigurations = false;
@@ -34,82 +33,82 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 550,
-  height: 550,
-  smoothing: false,
+    width: 550,
+    height: 550,
+    smoothing: false,
 };
 
 const gif = {
-  export: false,
-  repeat: 0,
-  quality: 100,
-  delay: 500,
+    export: false,
+    repeat: 0,
+    quality: 100,
+    delay: 500,
 };
 
 const text = {
-  only: false,
-  color: "#ffffff",
-  size: 20,
-  xGap: 40,
-  yGap: 40,
-  align: "left",
-  baseline: "top",
-  weight: "regular",
-  family: "Courier",
-  spacer: " => ",
+    only: false,
+    color: '#ffffff',
+    size: 20,
+    xGap: 40,
+    yGap: 40,
+    align: 'left',
+    baseline: 'top',
+    weight: 'regular',
+    family: 'Courier',
+    spacer: ' => ',
 };
 
 const pixelFormat = {
-  ratio: 2 / 128,
+    ratio: 2 / 128,
 };
 
 const background = {
-  generate: true,
-  brightness: "80%",
-  static: false,
-  default: "#000000",
+    generate: true,
+    brightness: '80%',
+    static: false,
+    default: '#000000',
 };
 
 const extraMetadata = {
-  external_url: "https://thecryptoeggs.com", // Replace with your website or remove this line if you do not have one.
+    external_url: 'https://thecryptoeggs.com', // Replace with your website or remove this line if you do not have one.
 };
 
-const rarityDelimiter = "#";
+const rarityDelimiter = '#';
 
 const uniqueDnaTorrance = 10000;
 
 const preview = {
-  thumbPerRow: 8,
-  thumbWidth: 50,
-  imageRatio: format.height / format.width,
-  imageName: "preview.png",
+    thumbPerRow: 8,
+    thumbWidth: 50,
+    imageRatio: format.height / format.width,
+    imageName: 'preview.png',
 };
 
 const preview_gif = {
-  numberOfImages: 8,
-  order: "ASC", // ASC, DESC, MIXED
-  repeat: 0,
-  quality: 100,
-  delay: 500,
-  imageName: "preview.gif",
+    numberOfImages: 8,
+    order: 'ASC', // ASC, DESC, MIXED
+    repeat: 0,
+    quality: 100,
+    delay: 500,
+    imageName: 'preview.gif',
 };
 
 module.exports = {
-  format,
-  baseUri,
-  description,
-  background,
-  uniqueDnaTorrance,
-  layerConfigurations,
-  rarityDelimiter,
-  preview,
-  shuffleLayerConfigurations,
-  debugLogs,
-  extraMetadata,
-  pixelFormat,
-  text,
-  namePrefix,
-  network,
-  gif,
-  preview_gif,
+    format,
+    baseUri,
+    description,
+    background,
+    uniqueDnaTorrance,
+    layerConfigurations,
+    rarityDelimiter,
+    preview,
+    shuffleLayerConfigurations,
+    debugLogs,
+    extraMetadata,
+    pixelFormat,
+    text,
+    namePrefix,
+    network,
+    gif,
+    preview_gif,
 };
